737f8e228e5702c7bc68a4d1af76eedaa1981d14
jungheejung <heejung.jung@colorado.edu>
2020-11-19 14:10:36 -0500

    update prompt message

--- scripts/RUN_faces.m
+++ scripts/RUN_faces.m
@@ -1,18 +1,17 @@
 
 % 1. grab participant number ___________________________________________________
 
-prompt = 'subject number (in raw number form, e.g. 1, 2,...,98): ';
+prompt = 'PARTICIPANT number (in raw number form, e.g. 1, 2,...,98): ';
 sub = input(prompt);
 
-prompt = ' run number (in raw number form, i.e. 1, 2, or 3): ';
+prompt = ' RUN number (1, 2, or 3): ';
 run = input(prompt);
 
+prompt = 'BIOPAC (YES=1 NO=0) : ';
+biopac = input(prompt);
 % 2. counterbalance version ____________________________________________________
 % random sequence
 r_seq =  [1,3,2,3,3,1,2,4,4,2,3];
 index= rem(sub,10)+1;
 f = ['task-faces_counterbalance_ver-0' num2str(r_seq(index+1))];
-faces(sub,f, run)
-
-
-
+faces(sub,f, run, biopac)
