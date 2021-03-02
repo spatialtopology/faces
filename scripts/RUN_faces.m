c87f4a8b203c5e42ce1f0053321f646aafdddfe5
jungheejung <heejung.jung@colorado.edu>
2021-03-01 22:04:31 -0500

    update run loop

--- scripts/RUN_faces.m
+++ scripts/RUN_faces.m
@@ -17,6 +17,7 @@ biopac = input(prompt);
 r_seq =  [1,3,2,3,3,1,2,4,4,2,3];
 index= rem(sub,10)+1;
 f = ['task-faces_counterbalance_ver-0' num2str(r_seq(index+1))];
-for run=1:3
-faces(sub,f, run, biopac);
+
+for r=run:3
+faces(sub,f, r, biopac);
 end
